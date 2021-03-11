8f170075836e14ed7685d728ac57de277e21a128
Zackory Kelly <zackkelly@live.ca>
2021-03-11 18:55:56 +0000

    Some stuff

---
+++ how-many-hundreds.js
@@ -0,0 +1,10 @@
+const howManyHundreds = function (number) {
+  number -= number % 100;
+  return number / 100;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
