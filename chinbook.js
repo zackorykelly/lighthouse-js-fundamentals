4eddecce97f66bd0195a396a786d03b490bcea7c
Zackory Kelly <zackkelly@live.ca>
2021-03-12 17:45:40 +0000

    updated the hyperspace modules

--- chinbook.js
+++ chinbook.js
@@ -4,13 +4,13 @@ var facebookProfile = {
   messages: ['poop', 'hello world', 'goodbye world'],
   postMessage: function (message) {
     facebookProfile.messages.push(message);
-  }
+  },
   deleteMessage: function (index) {
     facebookProfile.messages.splice(index, 1);
-  }
+  },
   addFriend: function () {
     facebookProfile.friends += 1;
-  }
+  },
   removeFriend: function () {
     facebookProfile.friends -= 1;
   }
