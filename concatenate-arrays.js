895465b70a8f80d265555b91377c6383f009380b
Zackory Kelly <zackkelly@live.ca>
2021-03-12 13:49:49 +0000

    smush some arrays together

---
+++ concatenate-arrays.js
@@ -0,0 +1,11 @@
+const concat = function (list1, list2) {
+  for (let i = 0; i < list2.length; i++) {
+    list1.push(list2[i]);
+  }
+  return list1;
+}
+
+console.log(concat([1, 2, 3], [4, 5, 6]), "=?", [1, 2, 3, 4, 5, 6]);
+console.log(concat([0, 3, 1], [9, 7, 2]), "=?", [0, 3, 1, 9, 7, 2]);
+console.log(concat([], [9, 7, 2]), "=?", [9, 7, 2]);
+console.log(concat([5, 10], []), "=?", [5, 10]);
